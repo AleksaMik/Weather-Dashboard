2ca3e20bc89711e3158b98e1523ad0746073dea0
AleksaMik <aleksam.2509@gmail.com>
2022-07-17 15:41:11 -0500

    5 days forecast function is created

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function getApi() {
 
 fetch (requestURL)
 .then(function (data) {
-    return data.JSON();
+    return data.json();
 })
 .then(function(data) {
 console.log(data);
@@ -34,25 +34,50 @@ function uvIndex(){
 const uvInUrl = "https://api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + longtitude + "&appid=45b6598a4a1bd706ba39bf0f2ac2fcf4";
 
 fetch (uvInUrl)
-.then(functon (datas) 
-    return datas.JSON();
-})
-.then(function (uvInUrl) {
+.then(functon (data) {
+  return data.json();
+}).then(function (uvInUrl) {
 console.log(uvInUrl.value);
 
 let uvInd = $("P");
 uvInd.text("UV index: " + uvInUrl.value).addClass("uvValue");
 if (uvInUrl.value <=3){
-    console.log (uvInUrl.value + "Low");
+    console.log (uvInUrl.value + " - Low");
 } else if (uvInUrl.value >3) {
-        console.log (uvInUrl.value + "Moderate");
+        console.log (uvInUrl.value + " - Moderate");
  } else if (uvInUrl.value <=7) {
-            console.log (uvInUrl.value + "Moderate");
+            console.log (uvInUrl.value + " - Moderate");
   } else {
-                console.log (uvInUrl.value + "High");
+                console.log (uvInUrl.value + " - High");
             }
-        }
+            $("#fetch").append(uvInd);
+        })
     }
-}
+    return uvIndex();
+
+})
 }
 
+function fiveDaysApi() {
+    const url5Days = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityPicked + "&units=imperial&appid=45b6598a4a1bd706ba39bf0f2ac2fcf4";
+fetch (url5Days)
+.then(function(data) {
+return data.json();
+}).then(function(data){
+    fetch("#5DaysForecast").empty();
+    console.log(data);
+    for (let i=0; i<40; i+=8){
+        let days = data.list [i];
+        let cards = document.getElementById('#5-days-forecast');
+        let cardInit = $("<div>").addClass("col-sm-4");
+        let cardDay = $("<h2>").text(days.dt_txt.slice(0, 10));
+                    let degree = $("<p>").text(Math.round(days.main.temp) + "˚F");
+                    let humid = $("<p>").text("Humidity: " + days.main.humidity + "%");
+                    let wind = $("<p>").text("wind Speed: " + Math.round(days.wind.speed) + " mph");
+                    let icon = $("<img>");
+                    icon.attr("src", "http://openweathermap.org/img/wn/" + days.weather[0].icon + "@2x.png");
+                    $("#fetch-five").append(cardInit.append(cardDay, degree, icon, humid, wind));
+                    $("#five-days").append(cards);
+    }
+})
+}
